e1b986d6de74d32287005390611dc4e0b4fb32d5
chopdvi1 <chopdevipul@gmail.com>
2026-08-25 12:18:21 +0530

    commited successfully

--- trigger.groovy
+++ trigger.groovy
@@ -1,5 +1,5 @@
 import jenkins.model.Jenkins
-def job = Jenkins.getInstance().getItemByFullName("java-app-deploy")
+def job = Jenkins.getInstance().getItemByFullName("frontend-deploy")
 if (job != null) {
     job.scheduleBuild2(0)
     println("Build triggered successfully from init script.")
